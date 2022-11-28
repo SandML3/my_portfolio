d034f299f499d2ac6b4155052c1dc38849064035
SandMl3 <sandramartinlop@gmail.com>
2022-11-28 11:27:33 +0100

    Complete home view

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,8 +1,9 @@
 import "../../styles/components/Home.scss";
+import myPotho from "../../images/myself.png";
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <header className="home__header">
         <div className="home__header__icon">
           <div className="home__header__icon__bar--top icon__bar"></div>
@@ -10,13 +11,8 @@ const Home = () => {
           <div className="home__header__icon__bar--bottom icon__bar"></div>
         </div>
       </header>
+
       <main className="home__main">
-        {/* <div className="home__main__stars">
-          <div className="star1 star"></div>
-          <div className="star2 star"></div>
-          <div className="star3 star"></div>
-          <div className="star4 star"></div>
-        </div> */}
         <section className="home__main__hero">
           <div className="home__main__hero__stars">
             <div className="star1 star"></div>
@@ -28,17 +24,58 @@ const Home = () => {
           <h2 className="home__main__hero__subtitle">Web developer</h2>
           <h1 className="home__main__hero__title">Sandra Martín</h1>
           <div className="home__main__hero__image__container">
-            <div className="home__main__hero__socialMedia"></div>
+            {/* <div className="home__main__hero__socialmedia">
+              <i class="fa-brands fa-github-alt"></i>
+            </div> */}
           </div>
         </section>
 
         <section className="home__main__about">
           <div className="home__main__about__bubble">
-            <p>Hola</p>
+            <p className="home__main__about__bubble__text">
+              Oh, oh, oh...
+              <span className="bolder uppercase">wait</span>.
+            </p>
+            <p className="home__main__about__bubble__text">Who?</p>
           </div>
+
+          <p className="home__main__about__text">Well, OK.</p>
+          <p className="home__main__about__text text--bold">
+            Let me introduce myself
+          </p>
+
+          <div className="arrow"></div>
+
+          <div className="star5 star"></div>
+
+          <div className="home__main__about__illustrationContainer">
+            <img
+              className="home__main__about__illustration"
+              src={myPotho}
+              alt="Selfportrait"
+              title="Selfportrait"
+            />
+          </div>
+
+          <p className="home__main__about__description">
+            Yep! that squiggle over there is me.
+          </p>
+          <p className="home__main__about__description">
+            I'm Sandra Martín, a newcomer{" "}
+            <span className="bolder uppercase highlight">web developer</span>.
+          </p>
+          <p className="home__main__about__description">
+            I'm a curious Sevillian passionate about graphic arts.
+          </p>
+          <p className="home__main__about__description"></p>
+          <p className="home__main__about__description">
+            I came to the world of programming attracted by its versatility and
+            I fell in love with it after discovering its incessant change and
+            its open and collaborative community.
+          </p>
         </section>
       </main>
-    </>
+    </div>
   );
 };
 export default Home;
